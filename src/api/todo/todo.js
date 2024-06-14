08055655111dfc055964daf4cdc3464d6c712cbf
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-14 08:36:46 -0300

    Mapeamento das Rotas

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -1,10 +1,12 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
+// const mongoose = require('../../config/database')
 
-const todoShema = new mongoose.Schema({
+const todoSchema = new mongoose.Schema({
     description: {type: String, required: true},
     done:{type: Boolean, required: true , default : false},
     createdAt: {type: Date, default: Date.now}
 })
 
-module.exports = result.model('Todo', todoShema)
+module.exports = restful.model('Todo', todoSchema)
+// module.exports = mongoose.model('Todo', todoSchema)
--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost:27017/todo',{useMongoClient:true})
+// module.exports = mongoose.connect('mongodb://localhost:27017/todo',{useMongoClient:true})
+module.exports = mongoose.connect("mongodb://localhost:27018/todo", {
+    useMongoClient: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+module.exports = mongoose;
 
 
-// const mongoose = require('mongoose');
-// mongoose.Promise = global.Promise
-// var uri = ('mongodb://127.0.0.1:27017/todo')
-// module.exports = mongoose.connect(uri,{useMongoClient:true});
-// module.exports= MongoClient.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   });
